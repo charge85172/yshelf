8b93a8d4d7f4e59e047386603132d97f34308205
tnvanbockel@gmail.com <tnvanbockel@gmail.com>
2025-09-22 12:56:52 +0200

    Making a includes map, with database include. by auth.php basic layout, and linked auth.php with index.html

--- php/auth.php
+++ php/auth.php
@@ -1,1 +1,24 @@
 <?php
+
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>The random thingy</title>
+</head>
+<body>
+<main>
+    <section id="choice">
+        <form action="" method="post">
+            <button type="submit" name="Register">registreer</button>
+            <button type="submit" name="Sign-In">inloggen</button>
+        </form>
+    </section>
+</main>
+</body>
+</html>
